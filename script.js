925c003fad71fcb3da333a3668d490c2a8764553
Gadelha <r.gadelhaa@gmail.com>
2025-02-17 23:04:32 -0300

    Install app v2

--- script.js
+++ script.js
@@ -32,8 +32,8 @@ let currentPage = 1;
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/service-worker.js')
-      .then(() => console.log("Service Worker registrado!"))
-      .catch((err) => console.log("Erro no Service Worker", err));
+      .then((reg) => console.log('Service Worker registrado!', reg))
+      .catch((err) => console.log('Erro no Service Worker:', err));
   });
 }
 
--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,21 @@
-self.addEventListener("install", (event) => {
-    event.waitUntil(
-      caches.open("app-cache").then((cache) => {
-        return cache.addAll([
-          "/",
-          "/index.html",
-          "/styles.css",
-          "/script.js",
-          "/LogoEquipe192x192.png",
-          "/LogoEquipe512x512.png"
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
-    );
-  });
+self.addEventListener('install', (event) => {
+  console.log('Service Worker instalado!');
+  event.waitUntil(
+    caches.open('rd-scout-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
